ec56d726d3605789b4a58c48a7a784f7e1d672e0
pmaclin <pcharlesantonio@gmail.com>
2015-03-24 00:46:54 -0500

    Adds relationships to all models

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,4 +1,8 @@
 class Batch < ActiveRecord::Base
   belongs_to :user
   belongs_to :recipe
+
+  has_many :reviews
+  has_many :uniques
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
   belongs_to :style
+
+  has_many :batches
 end
