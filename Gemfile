bb5543dcf6812372041ce04a63bac10b6d7f4d99
pmaclin <pcharlesantonio@gmail.com>
2015-05-26 14:12:07 -0500

    Adds avatar upload ability to user model; Edits user edit form (new session)
    Configures application controller to be able to receive avatar through
    mass assignment

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'carrierwave'
 gem 'kaminari'
 
 
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
+
+  mount_uploader :avatar, AvatarUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  # attr_accessible :email, :password, :remember_me, :avatar, :avatar_cache, :remove_avatar
 
   has_many :recipes
   has_many :batches
